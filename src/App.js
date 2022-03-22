27194627327687120ba9796dd809412acedcf2be
Adarsh Balila <adarshbalila17@gmail.com>
2022-03-22 10:21:41 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Weathertoday from './component/WeatherToday';
+
+function App() {
+  return (
+    <div>
+      <Weathertoday/>
+    </div>
+  );
+}
+
+export default App;
